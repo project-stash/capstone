bfda4b84aacfcc2a49cc83ae3927f48788c420e0
project-stash <jackhu0738@gmail.com>
2023-03-09 19:45:24 -0500

    component

---
+++ src/components/bookingpage.js
@@ -0,0 +1,5 @@
+function bookingpage(){
+    return
+}
+
+export default bookingpage;
--- src/components/main.js
+++ src/components/main.js
@@ -1,8 +1,14 @@
 import { Routes, Route } from 'react-router-dom';
 import './main.css';
+import bookingpage from './bookingpage.js'
+import homepage from './homepage.js'
 function Main(){
     return (
         <main>
+            <Routes>
+                <Route path="/booking" element={<bookingpage />} />
+                <Route path="/" element = {<homepage />} />
+            </Routes>
         </main>
     )
 }
